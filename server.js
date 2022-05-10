5dffe746a52e23871a3f4ebd600bcfeb28b9815f
jempitman <80392341+jempitman@users.noreply.github.com>
2022-05-10 16:40:12 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
+
+// // Set a local variable populated by the environment variable $NAME
+// const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function main() {
+//   while(true) {
+//     console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
+//     await sleep(5000);
+//   }
+// }
+
+// main();
